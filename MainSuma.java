d426537760b6078b3a4888332ed20a4dc707c18e
LocoLow02 <locolow03@gmail.com>
2024-04-23 22:12:34 -0600

    suma clase

---
+++ MainSuma.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MainSuma {
+    public static void main(String[] args) {
+        Suma n1 = new Suma();
+        Scanner num= new Scanner (System.in);
+        System.out.println("Suma de 2 numeros");
+        System.out.println("Ingrese un numero:");
+        int a= num.nextInt();
+        n1.a=a;
+        System.out.println("Ingrese segundo numero:");
+        int b= num.nextInt();
+        n1.b=b;
+        System.out.println("El resultado es:");
+        System.out.println(n1.sumar());
+    
+    }
+}
